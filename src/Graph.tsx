7e6fcddaafb355ed4edb331628750c4e699c4b32
Victoria Hale <vic.hale74@gmail.com>
2020-06-16 11:57:44 -0700

    add attributes to change graph view

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -34,6 +34,16 @@ class Graph extends Component<IProps, {}> {
     // Get element to attach the table from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
+    elem.setAttribute('view', 'y_line');
+    elem.setAttribute('column-pivots','["stock"]' );
+    elem.setAttribute('row-pivots', '["timestamp"]');
+    elem.setAttribute('columns', '["top_ask_price"]');
+    elem.setAttribute('aggregates', 
+      `{"stock":"distinct count",
+      "top_ask_price":"avg",
+      "top_bid_price":"avg",
+      "timestamp":"distinct count"}`);
+
     const schema = {
       stock: 'string',
       top_ask_price: 'float',
